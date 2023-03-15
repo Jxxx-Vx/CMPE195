7b1090f3379f1e54614a39e9a3c06f28faa488b6
John Vo <john.vo01@sjsu.edu>
2023-03-14 18:42:03 -0700

    Finished landing page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,7 +1,21 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
+
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    history.push('/register');
+  };
+
+  const handleSubmit2 = (event) => {
+    event.preventDefault();
+    history.push('/alerts');
+  };
+
   useEffect(() => {
     // Select all the elements that you want to animate
     const fadeElems = document.querySelectorAll('.fade-in');
@@ -40,7 +54,7 @@ const Home = () => {
               className="home-input"
               placeholder="Enter your email address"
             />
-            <button className="home-button">Sign Up</button>
+            <button className="home-button" onClick = {handleSubmit}>Sign Up</button>
           </form>
         </div>
         <div className="home-news">
@@ -48,7 +62,7 @@ const Home = () => {
           <p className="home-section-description">
             Take a look at the latest Emergency News reports
           </p>
-          <button className="home-section-button">View All News</button>
+          <button className="home-section-button" onClick={handleSubmit2}>View All News</button>
           <img
             src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/dbf4f428-9f63-44a4-9318-00cacdaadd2d/f3e72f14-c9c7-4dcb-bce0-bd9f8a254550?org_if_sml=1262002"
             alt="Emergency News"
@@ -56,40 +70,70 @@ const Home = () => {
           />
         </div>
         <div className="home-mission fade-in">
-          <h2 className="home-section-title">Our Mission</h2>
-          <p className="home-section-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
+          <div className="home-mission-text">
+            <h2 className="home-section-title">Our Mission</h2>
+              <p className="home-section-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum            
+              </p>
+          </div>
+          <div className="home-mission-image">
+            <img
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/dbf4f428-9f63-44a4-9318-00cacdaadd2d/f3e72f14-c9c7-4dcb-bce0-bd9f8a254550?org_if_sml=1262002"
+              alt="Our Mission"
+              className="home-section-image"
+            />
+          </div>
         </div>
+
+        
         <div className="home-how-it-works fade-in">
-          <h2 className="home-section-title">How it works?</h2>
-          <p className="home-section-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
+          <div className="home-how-text">
+            <h2 className="home-section-title">How it works?</h2>
+            <p className="home-section-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+          <div className="home-how-image">
+              <img
+                src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/dbf4f428-9f63-44a4-9318-00cacdaadd2d/f3e72f14-c9c7-4dcb-bce0-bd9f8a254550?org_if_sml=1262002"
+                alt=""
+                className="home-section-image"
+              />
+          </div>
         </div>
         <div className="home-about fade-in">
-          <h2 className="home-section-title">About the Team</h2>
-          <p className="home-section-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
+          <div className="home-about-text">
+            <h2 className="home-section-title">About the Team</h2>
+            <p className="home-section-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+
+          <div className="home-how-image">
+              <img
+                src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/dbf4f428-9f63-44a4-9318-00cacdaadd2d/f3e72f14-c9c7-4dcb-bce0-bd9f8a254550?org_if_sml=1262002"
+                alt=""
+                className="home-section-image"
+              />
+          </div>  
         </div>
       </div>
     </div>
