f3448180daad2989677799a8d215dd79c8e6122c
John Vo <john.vo01@sjsu.edu>
2023-03-14 18:10:28 -0700

    Edited Landing Page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,115 +1,99 @@
-import React from 'react'
-import './Home.css'
+import React, { useRef, useEffect } from 'react';
+import './Home.css';
 
-const Home = (props) => {
+const Home = () => {
+  useEffect(() => {
+    // Select all the elements that you want to animate
+    const fadeElems = document.querySelectorAll('.fade-in');
+
+    // Create a new Intersection Observer
+    const observer = new IntersectionObserver(entries => {
+      // Loop through each entry
+      entries.forEach(entry => {
+        // If the entry is visible
+        if (entry.isIntersecting) {
+          // Add the "is-visible" class to the element
+          entry.target.classList.add('is-visible');
+          // Stop observing the element
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    // Loop through each element and observe it
+    fadeElems.forEach(elem => {
+      observer.observe(elem);
+    });
+  }, []);
+
   return (
     <div className="home-container">
-      <div className="home-home-page">
-        <div className = "home-img1">
-          <div className = 'text1'>
-            <span>Get your Emergency News Here.</span>
-            <span>Sign up and subscribe to our newspaper.</span>
-            <img
-            src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/dbf4f428-9f63-44a4-9318-00cacdaadd2d/edee35b7-f005-4c71-b5ff-824fde88a790?org_if_sml=13593"
-            alt="SearchBarI633"
-            className="home-search-bar"
-          />
-          <button className="home-signinbutton">
-            Sign Up
-          </button>
-          </div>
-
-          <span className="home-text">
-            <span>Get your Emergency News Here.</span>
-          </span>
-          <span className="home-text02">
-            <span>Sign up and subscribe to our newspaper.</span>
-          </span>
+      <div className="home-content">
+        <div className="home-intro">
+          <h1 className="home-title">Get your Emergency News Here</h1>
+          <p className="home-description">
+            Sign up and subscribe to our newspaper.
+          </p>
+          <form className="home-form">
+            <input
+              type="text"
+              className="home-input"
+              placeholder="Enter your email address"
+            />
+            <button className="home-button">Sign Up</button>
+          </form>
         </div>
-        <div className = "curve"></div>
-        <div className="home-navigateto-emergency-news">
-          <div className="home-navigateto-news"/>
-          <span className="home-text06">
-            <span className="home-text07">
-              <span>Take a look at the latest Emergency News reports</span>
-              <br></br>
-              <span></span>
-              <br></br>
-              <span></span>
-            </span>
-          </span>
-          <div className="home-navigateto-news-button">
-            <span className="home-text13">
-              <span> Emergency News</span>
-            </span>
-          </div>
-          <span className="home-text15">
-            <span>
-              Creating News Report Through Twitter API
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: ' ',
-                }}
-              />
-            </span>
-          </span>
+        <div className="home-news">
+          <h2 className="home-section-title">Emergency News</h2>
+          <p className="home-section-description">
+            Take a look at the latest Emergency News reports
+          </p>
+          <button className="home-section-button">View All News</button>
           <img
             src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/dbf4f428-9f63-44a4-9318-00cacdaadd2d/f3e72f14-c9c7-4dcb-bce0-bd9f8a254550?org_if_sml=1262002"
-            alt="image4I633"
-            className="home-image4"
+            alt="Emergency News"
+            className="home-section-image"
           />
         </div>
-        <div className="home-mission">
-          <span className="home-text17">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </span>
-          </span>
-          <span className="home-text19">
-            <span>Our Mission</span>
-          </span>
+        <div className="home-mission fade-in">
+          <h2 className="home-section-title">Our Mission</h2>
+          <p className="home-section-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
         </div>
-        <div className="home-howit-works">
-          <span className="home-text21">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </span>
-          </span>
-          <span className="home-text23">
-            <span>How it works?</span>
-          </span>
+        <div className="home-how-it-works fade-in">
+          <h2 className="home-section-title">How it works?</h2>
+          <p className="home-section-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
         </div>
-        <div className="home-aboutthe-team">
-          <span className="home-text25">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </span>
-          </span>
-          <span className="home-text27">
-            <span>About the Team</span>
-          </span>
+        <div className="home-about fade-in">
+          <h2 className="home-section-title">About the Team</h2>
+          <p className="home-section-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
