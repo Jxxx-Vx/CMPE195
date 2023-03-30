d7e0c241cace7fd6b5920b088f748fd0074f1668
Phuoc Le <phuocle3210@gmail.com>
2023-03-30 13:14:55 -0700

    added usgs alerts

--- server/controllers/alerts.js
+++ server/controllers/alerts.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 export const usgs = async (req, res) => {
-
-    const r = req.body;
-    try {
-      const response = await axios.get("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-03-09&limit=3&orderby=time").then((response) => {
-        console.log("USGS API CALL", response.data);
-        res.json(response.data); 
+  const r = req.body;
+  try {
+    const response = await axios
+      .get(
+        "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-03-09&limit=12&orderby=time"
+      )
+      .then((response) => {
+        res.json(response.data);
       });
-    } catch (err) {
+  } catch (err) {
     console.log("Alert Error: ", err);
   }
 };
-  
+
 export const nws = async (res) => {};
-  
