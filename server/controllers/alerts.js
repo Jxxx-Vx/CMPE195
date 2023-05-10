bb8de3a2df565074c021bdff36dd0af189ee9833
John Vo <john.vo01@sjsu.edu>
2023-05-10 11:37:48 -0700

    Adding backend stuff

--- server/controllers/alerts.js
+++ server/controllers/alerts.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 export const usgs = async (req, res) => {
-  const r = req.body;
+
+  
+  const{lng, lat} = req.body;
+
+  //https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=37.7749&longitude=-122.4194&maxradiuskm=100
+
   try {
     const response = await axios
       .get(
-        "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-03-09&limit=12&orderby=time"
+        `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-03-09&limit=12&orderby=time&&latitude=${lat}&longitude=${lng}&maxradiuskm=100`
       )
       .then((response) => {
         res.json(response.data);
@@ -16,11 +21,13 @@ export const usgs = async (req, res) => {
 };
 
 export const nws = async (req, res) => {
-    const{area} = req.body;
+  
+  const{lng, lat} = req.body;
+
     try {
       const response = await axios
         .get(
-          `https://api.weather.gov/alerts/active?limit=20&area=${area}`
+          `https://api.weather.gov/alerts/active?limit=20&point=${lat},${lng}`
         )
         .then((response) => {
           res.json(response.data);
